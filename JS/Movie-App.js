ac2fe9efe1ceaf36444f414c2d2dc03ab335aea9
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-08 14:33:20 -0500

    Added gitignore file, css directory and stylesheet, card carousel in HTML, and TMDB fetch in js file.

--- JS/Movie-App.js
+++ JS/Movie-App.js
@@ -1,4 +1,37 @@
 'use strict';
 
+(function(){
 
+    function getMovieData () {
+        return fetch(`https://freckle-attractive-group.glitch.me/movies`)
+            .then((resp) => resp.json())
+            .then((data => {
+                console.log(data);
+            }));
+    }
+
+    const options = {method: 'GET',
+        headers: {accept: 'application/json'}};
+
+    fetch('https://api.themoviedb.org/3/67da7a691d4363302b9007f99d6f16e2', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+
+
+    getMovieData()
+
+
+
+
+
+
+
+
+
+
+
+
+})();
 
