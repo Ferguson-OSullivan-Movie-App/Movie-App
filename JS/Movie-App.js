f0e8e848f3f76bed76f5e90fc8dbfebdf5996ca3
Brenden <brenden.m.ferguson@gmail.com>
2023-06-09 12:33:46 -0500

    Working on adding images to the html

--- JS/Movie-App.js
+++ JS/Movie-App.js
@@ -5,30 +5,39 @@
     $(document).ready(function () {
 
         let movieArray = [];
+        let moviePoster = [];
 
         function getMovieData() {
             return fetch(`https://freckle-attractive-group.glitch.me/movies`)
                 .then((resp) => resp.json())
                 .then((data => {
                     for (let i = 0; i <= data.length - 1; i++) {
-                        console.log(data[i]);
-                        console.log(data[i].id);
-                        console.log(data[i].rating);
-                        console.log(data[i].title);
                         movieArray.push(data[i])
+                        fetch(`http://www.omdbapi.com/?t=${data[i].title}&apikey=${OMDB}`)
+                        .then((resp) => resp.json())
+                        .then((data) => {
+                            moviePoster.push(data.Poster);
+                            console.log(data.Poster)
+                            console.log(moviePoster)
+                        })
+                        // console.log(data[i]);
+                        // console.log(data[i].id);
+                        // console.log(data[i].rating);
+                        // console.log(data[i].title);
                     }
                     $('#movie-card').html(renderMovies(movieArray));
                 }));
 
         }
 
+
+
         getMovieData();
 
         // let movieDataString = '';
         console.log(movieArray);
 
-        // buildMovieData(movieArray);
-
+        // buildMovieData(movieArray)
         function buildMovieData(obj) {
             console.log(movieArray);
             let movieDataString = '';
@@ -36,18 +45,19 @@
                 movieDataString += `<div class=" card-body>`
                 movieDataString += `<h5 class="card-title"> ${obj.rating} </h5>`
                 movieDataString += `<h5 class="card-title"> ${obj.title} </h5>`
-
                 movieDataString += '</div>'
                 movieDataString += '</div>'
             console.log(movieDataString);
             return movieDataString;
         }
 
-
+        console.log(moviePoster[0])
         function renderMovies(objElement) {
             let movieDataString = '';
             for (let i= 0; i < objElement.length; i++) {
                 movieDataString += buildMovieData(objElement[i])
+                // console.log(moviePoster[0])
+                // movieDataString += `<img class="poster" src=${moviePoster[i]} alt="">`
             }
             return movieDataString
         }
@@ -70,15 +80,12 @@
 
         function userMovieSearch () {
             let userSearchInput = $('#search-box').val();
-
             fetch(`http://www.omdbapi.com/?t=${userSearchInput}&apikey=${OMDB}`)
                 .then(response => response.json())
                 .then(response => console.log(response))
                 .catch(err => console.error(err));
-                movieArray.push(userSearchInput.title);
+                // movieArray.push(userSearchInput.title);
         }
-
-
     });
 })();
 
